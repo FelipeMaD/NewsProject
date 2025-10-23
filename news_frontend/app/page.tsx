18c1b2cd14147d2ecffd377c00745582d76d1120
FelipeMaD <felipemarinhodr@hotmail.com>
2025-10-23 15:22:12 -0300

    Start: Projeto NewsMar

---
+++ news_frontend/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
+        <p>
+          Criando projeto do zero
+        </p>
+    </div>
+  );
+}
